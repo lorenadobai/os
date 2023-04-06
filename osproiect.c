f231d595642bc3e5f97fc7ddec9a93dfe6a4df57
lorenadobai <93092056+lorenadobai@users.noreply.github.com>
2023-04-06 21:42:30 +0300

    Add files via upload

---
+++ osproiect.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <ctype.h> 
+#include <errno.h> 
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc,char *argv[])
+{
+     if(argc!=3)
+   {
+    perror("not enough arguments");
+    exit(2);
+   }
+}
