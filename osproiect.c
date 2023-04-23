bf68b727f9d159bddeb1296d0c5ab06f8029d9e1
Lorena Dobai <lorena.dobai@student.upt.ro>
2023-04-23 20:14:15 +0300

    started the os project

--- osproiect.c
+++ osproiect.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 int main(int argc,char *argv[])
 {
@@ -13,6 +14,5 @@ int main(int argc,char *argv[])
    {
     perror("not enough arguments");
     exit(2);
-    
    }
 }
