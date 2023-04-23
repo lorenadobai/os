fd872df454572a46e980cc73e428985603a7283c
Lorena Dobai <lorena.dobai@student.upt.ro>
2023-04-23 20:16:56 +0300

    started the os project

--- osproiect.c
+++ osproiect.c
@@ -10,9 +10,34 @@
 
 int main(int argc,char *argv[])
 {
-     if(argc!=3)
+    FILE *fisOut;
+  struct stat info;
+   if(argc!=3)
    {
     perror("not enough arguments");
     exit(2);
    }
+   if(fisOut=open(argv[2], O_RDONLY)<0){
+        perror("fisOut cannot be opened!\n");
+        exit(0);
+   }
+   if(fstat(fisOut,&info)<0){
+        perror("fstat dind't work\n");
+        exit(0);
+   }
+    if(!(S_ISREG(info.st_mode))){
+        perror("S_ISREG dind't work\n");
+        exit(0);
+   }
+   if( stat( fisOut, &info) != -1) 
+{
+    if( S_ISREG( info.st_mode ) != 0 )
+    {
+        printf( "%s is a file", fisOut ) ;
+    }
+    else
+    {
+        printf( "%s is not a file", fisOut ) ;
+    }
+}
 }
