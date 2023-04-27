06cb7abfa19dd89ab3f8ee901c5544473d753d56
Lorena Dobai <lorena.dobai@student.upt.ro>
2023-04-27 21:09:43 +0300

    got new ideas,deleted the old ones

--- osproiect.c
+++ osproiect.c
@@ -7,37 +7,82 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <wait.h>
+char path[1024];
+char result[1024];
+
+void RegularFileName(FILE *f)
+{
+    int fd = fileno(f); 
+    sprintf(path, "/proc/self/fd/%d", fd);
+    memset(result, 0, sizeof(result));
+    readlink(path, result, sizeof(result)-1);
+    printf("%s\n", result);
+}
+
+void RegularFileSize(FILE *f)
+{
+    struct stat st;
+    stat(f, &st);
+    int size = st.st_size;
+}
+
+void RFHardLinkCount(FILE *f)
+{
+    struct stat st;
+    if (stat(f, &st) == -1) 
+    {
+        perror("stat doesn't funtion for hard links");
+        exit(0);
+    }
+    else 
+        printf("%s has %d hard links\n", f, st.st_nlink);
+}
+
+void RFTimeModification(FILE *f)
+{
+
+}
+
+void RFAccess(FILE *f)
+{
+
+}
+
+void RFSymbolic(FILE *f)
+{
+
+}
 
 int main(int argc,char *argv[])
 {
-    FILE *fisOut;
-  struct stat info;
-   if(argc!=3)
-   {
-    perror("not enough arguments");
-    exit(2);
-   }
-   if(fisOut=open(argv[2], O_RDONLY)<0){
+    FILE *fis;
+    DIR *dir;
+    struct stat info;
+    if(argc<=2)
+    {
+        perror("not enough arguments");
+        exit(2);
+    }
+    if(fis=open(argv[2], O_RDONLY)<0){
         perror("fisOut cannot be opened!\n");
         exit(0);
-   }
-   if(fstat(fisOut,&info)<0){
-        perror("fstat dind't work\n");
-        exit(0);
-   }
-    if(!(S_ISREG(info.st_mode))){
-        perror("S_ISREG dind't work\n");
-        exit(0);
-   }
-   if( stat( fisOut, &info) != -1) 
-{
-    if( S_ISREG( info.st_mode ) != 0 )
-    {
-        printf( "%s is a file", fisOut ) ;
     }
     else
     {
-        printf( "%s is not a file", fisOut ) ;
+        char c;
+        printf("choose one for files: name (-n), size (-d), hard link count (-h), time of last modification (-m), access rights (-a), create symbolic link (-l, this will wait for user input for the name of the link).");
+        scanf("%c",c);
+        switch (c)
+        {
+        case '-n': RegularFileName(fis); break;
+        case '-d': RegularFileSize(fis); break;
+        case '-h': RFHardLinkCount(fis); break;
+        case '-m': RFTimeModification(fis); break;
+        case '-a': RFAccess(fis); break;
+        case '-l': RFSymbolic(fis); break;
+        default:
+            break;
+        }
     }
-}
 }
