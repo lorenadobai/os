a3faad6cfb4113ee72485e1a8e14eac773f0ee1e
Lorena Dobai <lorena.dobai@student.upt.ro>
2023-05-12 19:11:34 +0300

    created some pipes. final project

--- osproiect.c
+++ osproiect.c
@@ -1,19 +1,25 @@
-#include <stdlib.h>
-#include <ctype.h> 
-#include <errno.h> 
-#include <fcntl.h> 
-#include <unistd.h> 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <dirent.h>
-#include <wait.h>
+#include <stdbool.h>
+#include <sys/wait.h>
 char path[1024];
 char result[1024];
 char buffer[100];
 struct stat info;
 
 #define bufferSize 200
+
+struct errorAndWarning{
+    int errors, warnings;
+};
 
 void RFTimeModification(char *path)
 {
@@ -70,9 +76,80 @@ void Access(struct stat var)
         (var.st_mode & S_IXOTH) ? "yes" : "no");
 }
 
+bool checkCF(char *name){
+    char *extension = strrchr(name,'.');
+    if(extension && !strcmp(extension,".c")){
+            return true;
+        }
+    return false;
+}
+void checkEAndW(char *name, int createPipe[]){
+    
+    char buffer[1024];
+    char command[1024];
+    sprintf(command,"./script.sh %s", name);
+
+    FILE* pipe= popen(command,"r");
+    if(!pipe){
+        perror("Couldn't open the pipe for script.sh");
+        exit(0);
+    }
+    
+    printf("\n");
+
+
+    int ok = 1;
+    int warnings = 0, errors = 0;
+    while(fgets(buffer, 1024, pipe)){
+        for(int i=0;i<strlen(buffer);i++){
+            if(isdigit(buffer[i])){
+                if(ok == 1){
+                    warnings = warnings * 10 + buffer[i]-'0';
+                }
+                else{
+                    errors = errors * 10 + buffer[i]-'0';
+                }
+            }
+        }
+        ok = 0;
+    }
+
+    struct errorAndWarning Data;
+    Data.warnings = warnings;
+    Data.errors = errors;
+
+    close(createPipe[0]);
+    write(createPipe[1],&Data,sizeof(Data));
+    close(createPipe[1]);
+
+    pclose(pipe);
+}
+
+void countLines(FILE *fis){
+    int count;
+    fscanf(fis,"%d",&count);
+    
+    printf("The file contains %d lines\n",count);
+    pclose(fis);
+}
+
+int score2(struct errorAndWarning Data){
+    int score;
+    if(!Data.errors && !Data.warnings)
+        score = 10;
+        else if(Data.errors>0)
+            score = 1;
+            else if(!Data.errors && Data.warnings>10)
+                score = 2;
+                else if(!Data.errors && Data.warnings<=10)
+                    score = 2+8*(10-Data.warnings)/10;
+    return score;
+}
 
 void File(char* name, struct stat var)
 {
+    char options[10];
+    int status;
     FILE *fis=fopen(name, "r");
     if(fis==NULL){
         perror("fis cannot be opened!\n");  
@@ -88,7 +165,7 @@ void File(char* name, struct stat var)
     fprintf(stderr, "Failed to fork\n.");
             exit(2);
     }
-    for(int i=0;i<6; i++)
+    for(int i=0;i<strlen(options)-1; i++)
     {
         if(pid==0){
             switch (j)
@@ -102,15 +179,78 @@ void File(char* name, struct stat var)
                 default: printf("it is not a choice");
                 break;
             }
+            exit(2);
+        }else{
+            if(i==strlen(options)-2){
+                    
+                for (int i = 0; i < strlen(options)-2; i++) {
+                    pid_t child = waitpid(-1, &status, 0);
+
+                    if (WIFEXITED(status)) {
+                        int exit_code = WEXITSTATUS(status);
+                        printf("\nThe process with PID %d has ended with the exit code %d\n", child, exit_code);
+                    } else {
+                        printf("\nThe process with PID %d has ended not normal\n", child);
+                    }
+                }
+                int createPipe[2];
+                if(pipe(createPipe) == -1){
+                    perror("Couldn't create pipe\n");
+                    exit(0);
+                }
+                pid_t pid2;
+                pid2 = fork();
+
+                if(pid2==0){
+                    countLines(fis); 
+                    exit(0);
+                } else{
+                        
+                    if(checkCF(name)){
+                        close(createPipe[1]);
+                        struct errorAndWarning data;
+                        int len = read(createPipe[0], &data, sizeof(data));
+                        if(len!=sizeof(data)){
+                            perror("Couldn't read from pipe\n");
+                            exit(0);
+                        }
+                        else{
+                            int score = score2(data);
+                            FILE *f = fopen("grades.txt","a+");
+                            if(!f){
+                                perror("Couldn't open the grades file.\n");
+                                exit(0);
+                            }
+                            else{
+                                printf("Score was successfully written.\n");
+                            }
+                            fprintf(f,"File has the score: %d\n",score);
+                        }
+                        close(createPipe[0]);
+                    }
+                    wait(&status);
+                    if (WIFEXITED(status)) {
+                        int exit_code = WEXITSTATUS(status);
+                        printf("\nThe process with PID %d has ended with the exit code %d\n", pid2, exit_code);
+                    } else {
+                        printf("\nThe process with PID %d has ended abnormally\n", pid2);
+                    }
+                        
+                }
+            }
+                
         }
     }
-}
+    fclose(fis); 
+} 
 
 void Directory(char* name, struct stat var)
 {
     DIR *dir=opendir(name);
+    char options[10];
     struct dirent* entry;
     struct stat st;
+    int status;
     char file[bufferSize];
     int totalFiles = 0;
     if(dir==NULL)
@@ -140,7 +280,7 @@ void Directory(char* name, struct stat var)
         fprintf(stderr, "Failed to fork\n.");
         exit(2);
     }
-    for(int i=0;i<4; i++)
+    for(int i=0;i<strlen(options); i++)
     {
         if(pid==0){
             switch (j)
@@ -152,6 +292,20 @@ void Directory(char* name, struct stat var)
                 default: printf("it is not a choice");
                 break;
             }
+            exit(2);
+        }else{
+            if(i==strlen(options)-2){
+                for (int i = 0; i < strlen(options)-1; i++) {
+                    pid_t child = waitpid(-1, &status, 0);
+
+                    if (WIFEXITED(status)) {
+                        int exit_code = WEXITSTATUS(status);
+                        printf("\nThe process with PID %d has ended with the exit code %d\n", child, exit_code);
+                    } else {
+                        printf("\nThe process with PID %d has ended not normal\n", child);
+                    }
+                }
+            }
         }
     }
     closedir(dir);
@@ -159,6 +313,8 @@ void Directory(char* name, struct stat var)
 
 void SymbolicLink(char* name, struct stat var)
 {
+    int status;
+    char options[10];
     if (lstat(name, &var) == -1)
     {
         perror("lstat");
@@ -174,7 +330,7 @@ void SymbolicLink(char* name, struct stat var)
         fprintf(stderr, "Failed to fork\n.");
         exit(2);
     }
-    for(int i=0;i<5; i++)
+    for(int i=0;i<strlen(options); i++)
     {
         if(pid==0){
             switch (j)
@@ -192,7 +348,21 @@ void SymbolicLink(char* name, struct stat var)
                 default: printf("it is not a choice");
                 break;
             }
-        } 
+            exit(2);
+        } else{
+            if(i==strlen(options)-2){
+                for (int i = 0; i < strlen(options)-1; i++) {
+                    pid_t child = waitpid(-1, &status, 0);
+
+                    if (WIFEXITED(status)) {
+                        int exit_code = WEXITSTATUS(status);
+                        printf("\nThe process with PID %d has ended with the exit code %d\n", child, exit_code);
+                    } else {
+                        printf("\nThe process with PID %d has ended not normal\n", child);
+                    }
+                }
+            }
+        }
     }
 }
 
